b74da555dcc5e7a94edfe9f1b86605b217a1b9d0
feng-jianjun <63890178+feng-jianjun@users.noreply.github.com>
2021-12-11 17:38:36 +0800

    80加入Eureka注册中心

--- snut-order80/src/main/java/org/snut/order/OrderApplication.java
+++ snut-order80/src/main/java/org/snut/order/OrderApplication.java
@@ -2,6 +2,7 @@ package org.snut.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author curtis
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
