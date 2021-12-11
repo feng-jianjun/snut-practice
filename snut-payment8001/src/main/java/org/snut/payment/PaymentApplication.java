53629509afa7e258bcc29e7b12ff59977a275042
feng-jianjun <63890178+feng-jianjun@users.noreply.github.com>
2021-12-11 17:29:21 +0800

    8001加入Eureka注册中心

--- snut-payment8001/src/main/java/org/snut/payment/PaymentApplication.java
+++ snut-payment8001/src/main/java/org/snut/payment/PaymentApplication.java
@@ -2,6 +2,7 @@ package org.snut.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author curtis
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
